178ff6e03a01e6ae29d35599df8f2b38b58eb1fd
Inklay <inklay@outlook.fr>
2026-04-07 23:11:22 +0200

    Added SADL conversion with vgmstream

--- source/FileFormat/Conversion/SADL.cpp
+++ source/FileFormat/Conversion/SADL.cpp
@@ -1,9 +1,21 @@
 #include "FileFormat/Conversion/SADL.h"
+#include <sstream>
 
 void SADL::convert(const fileUtils::path& folderPath, const fileUtils::path& outputFolder) {
-	convertFileFromFolder(folderPath, outputFolder, ".sad", convertToWav);
+	convertFileFromFolder(folderPath, outputFolder, ".SAD", convertToWav);
 }
 
 void SADL::convertToWav(const fileUtils::path& filePath, const fileUtils::path& outputFolder, bool isCompressed) {
+	std::stringstream stream;
 
+#ifdef _WIN32
+	const std::string ffmpeg = "bin\\vgmstream-cli.exe";
+	const std::string nullOutput = " > nul 2>&1";
+#else
+	const std::string ffmpeg = "./bin/vgmstream";
+	const std::string nullOutput = " > /dev/null 2>&1";
+#endif
+
+	stream << ffmpeg << " -o" << outputFolder / filePath.filename().replace_extension("wav") << " " << std::filesystem::absolute(filePath) << nullOutput;
+	system(stream.str().c_str());
 }
