4f228a24268085deda899e40d753523f78ef72b6
Inklay <inklay@outlook.fr>
2026-05-06 01:26:19 +0200

    Fixed video file path in cinematics

--- source/Game/Layton1/Scene/Cinematic.cpp
+++ source/Game/Layton1/Scene/Cinematic.cpp
@@ -1,19 +1,22 @@
 #include "Game/Layton1/Scene/Cinematic.h"
 #include "Game/Game.h"
-#include <libavformat/avformat.h>
-#include <libavcodec/avcodec.h>
-#include <libswscale/swscale.h>
+
+extern "C" {
+	#include <libavformat/avformat.h>
+	#include <libavcodec/avcodec.h>
+	#include <libswscale/swscale.h>
+}
 
 namespace Layton1Scene {
 	Cinematic::Cinematic(Game* game, const fileUtils::path& videoFile, const fileUtils::path& audioFile) :
 		Scene(game),
 		m_audioFile(audioFile)
 	{
-		//AVFormatContext* fmtCtx = NULL;
-		//avformat_open_input(&fmtCtx, videoFile.string().c_str(), NULL, NULL);
-		//avformat_find_stream_info(fmtCtx, NULL);
+		AVFormatContext* fmtCtx = NULL;
+		avformat_open_input(&fmtCtx, (m_game->m_gameFolder / videoFile).string().c_str(), NULL, NULL);
+		avformat_find_stream_info(fmtCtx, NULL);
 
-		//int videoStream = av_find_best_stream(fmtCtx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+		int videoStream = av_find_best_stream(fmtCtx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
 	}
 
 	void Cinematic::load() {
